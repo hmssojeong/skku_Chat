c85856e56f47602c17bb1e0c6ca93b484012e86c
hmssojeong <sojeong9638@gmail.com>
2026-03-09 14:26:16 +0900

    Asset: PunChatAsset

---
+++ Assets/02.Scripts/ChatManager.cs
@@ -0,0 +1,79 @@
+using ExitGames.Client.Photon;
+using Photon.Chat;
+using UnityEngine;
+
+
+public class ChatManager : MonoBehaviour, IChatClientListener
+{
+    public static ChatManager Instance { get; private set; }
+
+    // 채팅 클라이언트
+    private ChatClient _chatClient;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        _chatClient = new ChatClient(this);
+        _chatClient.ChatRegion = "ASIA";
+    }
+
+    public void DebugReturn(DebugLevel level, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDisconnected()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnConnected()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnChatStateChange(ChatState state)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnGetMessages(string channelName, string[] senders, object[] messages)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPrivateMessage(string sender, object message, string channelName)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSubscribed(string[] channels, bool[] results)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnsubscribed(string[] channels)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUserSubscribed(string channel, string user)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUserUnsubscribed(string channel, string user)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
